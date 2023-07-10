98c2259a545657c18d647f3874e9eb9469f5b6dd
annamoskviceva <Annab.spb@mail.ru>
2023-07-10 18:14:20 +0300

    дз и доп пакет

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -2,6 +2,21 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author dostoevsky = new Author("Фёдор", "Достоевский");
+        Book prestuplenieNakazanie = new Book ("Преступление и наказание", dostoevsky,1866);
+        Author gogol = new Author("Николай","Гоголь");
+        Book mertveyDushi = new Book ("Мёртвые души", gogol, 1835);
+
+        System.out.println(prestuplenieNakazanie.getName());
+        System.out.println(prestuplenieNakazanie.getYear());
+        prestuplenieNakazanie.setYear(1866);
+        System.out.println(prestuplenieNakazanie.getYear());
+
+        System.out.println(mertveyDushi.getName());
+        System.out.println(mertveyDushi.getYear());
+        mertveyDushi.setYear(1835);
+        System.out.println(mertveyDushi.getYear());
+
+
     }
 }
